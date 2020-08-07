6923f95e1d4d2aa28eb0bad9a90c71a0da281d20
Takuro Ashie <ashie@clear-code.com>
2020-08-07 13:25:54 +0900

    Add more tests for Redmine::Comment

--- test/test_redmine.rb
+++ test/test_redmine.rb
@@ -1,13 +1,44 @@
 require "test/unit"
 require "json"
+require "time"
 require_relative "../lib/gitredhubmine/redmine"
 
 class TestRedmineComment < Test::Unit::TestCase
   include GitRedHubMine
 
+  TEST_COMMENT = {
+    id: 13,
+    created_on: Time.parse("2020-08-06 10:41:42 +0000"),
+    updated_on: Time.parse("2020-08-06 10:52:12 +0000"),
+    notes: "hoge"
+  }
+
+  setup do
+    obj = JSON.parse(TEST_COMMENT.to_json)
+    @comment = Redmine::Comment.new(obj)
+  end
+
+  test "tracker" do
+    assert_equal("Redmine", @comment.tracker)
+  end
+
   test "body" do
-    json = { notes: "hoge" }.to_json
-    a = Redmine::Comment.new(JSON.parse(json))
-    assert_equal("hoge", a.body)
+    assert_equal("hoge", @comment.body)
+  end
+
+  test "created_at" do
+    assert_equal(TEST_COMMENT[:created_on], @comment.created_at)
+  end
+
+  test "updated_at" do
+    assert_raise(NotImplemented) do
+      assert_equal(TEST_COMMENT[:updated_on], @comment.updated_at)
+    end
+  end
+
+  test "user" do
+    assert_raise(NotImplemented) do
+      assert_equal(TEST_COMMENT[:updated_on], @comment.user)
+    end
   end
 end
